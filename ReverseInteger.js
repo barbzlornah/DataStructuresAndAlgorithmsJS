c7884b2d51c2af2ffdcc15a9af208135eba2c82e
Barbara Masinde <barbzlornah@gmail.com>
2021-01-05 11:39:41 +0300

    new commit

---
+++ ReverseInteger.js
@@ -0,0 +1,37 @@
+// Given a 32-bit signed integer, reverse digits of an integer.
+
+// Note:
+// Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+ 
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+// Example 4:
+
+// Input: x = 0
+// Output: 0
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+
+const reverse = (x) => {
+    if(x < 0) return -1 * reverse(-x);
+
+    // let reverseInt = (x + "").split("").reverse().join("");
+    let reverseInt = Number(x.toString().split("").reverse().join(""));
+    return (reverseInt > 2 **31-1)? 0: reverseInt;
+};
+console.log(reverse(x = 123));
